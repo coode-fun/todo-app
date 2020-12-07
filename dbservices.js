26f1db3fe1ab6c830d2ab7ed171b4cc48e8fd432
coode-fun <gbye8334@gmail.com>
2020-12-08 01:35:48 +0530

    om

--- dbservices.js
+++ dbservices.js
@@ -7,7 +7,7 @@ dotenv.config();
 
 const connection=mysql.createConnection({
     host:process.env.HOST,
-    user:'root',
+    user:process.env.USER,
     password:process.env.PASSWORD,
     database:process.env.DATABASE,
     port:process.env.DBPORT

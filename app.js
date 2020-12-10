ab46a699f8740155bdccfcf82d2f200e48f97445
coode-fun <gbye8334@gmail.com>
2020-12-10 14:15:40 +0530

    removed proxy field in package.json

--- app.js
+++ app.js
@@ -111,6 +111,6 @@ app.delete("/delete/:Id",(request,response)=>{
     .catch(err => console.log(err));
 });
 var port=process.env.PORT||5000;
-app.listen(5000,()=>{
+app.listen(port,()=>{
     console.log(`Server Running at http://localhost:${port}`);
 });

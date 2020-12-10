e3dab091ffd95dd4dd9cda247c65051ad1e9ca86
coode-fun <gbye8334@gmail.com>
2020-12-10 12:46:00 +0530

    app.js

--- app.js
+++ app.js
@@ -16,22 +16,22 @@ app.use(express.static(__dirname+"/client"));
 //     res.sendFile(path.resolve(__dirname,'public',''))
 // })
 //create
-const allowedOrigins = ["http://localhost:5000","http://localhost:8080"];
+// const allowedOrigins = ["http://localhost:5000","http://localhost:8080"];
 
-    app.use(
-        cors({
-            origin: function(origin, callback) {
-                if (!origin) return callback(null, true);
-                if (allowedOrigins.indexOf(origin) === -1) {
-                    var msg =
-                        "The CORS policy for this site does not " +
-                        "allow access from the specified Origin.";
-                    return callback(new Error(msg), false);
-                }
-                return callback(null, true);
-            }
-        })
-    );
+//     app.use(
+//         cors({
+//             origin: function(origin, callback) {
+//                 if (!origin) return callback(null, true);
+//                 if (allowedOrigins.indexOf(origin) === -1) {
+//                     var msg =
+//                         "The CORS policy for this site does not " +
+//                         "allow access from the specified Origin.";
+//                     return callback(new Error(msg), false);
+//                 }
+//                 return callback(null, true);
+//             }
+//         })
+//     );
 app.post("/insert",(request,response)=>{
 
     if(!request.body)
